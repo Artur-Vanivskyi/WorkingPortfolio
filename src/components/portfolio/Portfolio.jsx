07591a6b92e53121ad42b4f779d22f7fc3f56b0d
Artur <vanivskyi.artur@thinkful.com>
2022-11-22 13:56:55 -0500

    added tech elements to projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./portfolio.css";
 import {data} from "../../data/data";
 
+
 const Portfolio = () => {
   return (
     <section id="portfolio">
@@ -9,19 +10,21 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        {data.map(({ id, image, title, github, demo }) => {
+        {data.map(({ id, image, title, github, livesite, text, tech }) => {
           return (
             <article key={id} className="portfolio__item">
               <div className="portfolio__item-image">
                 <img src={image} alt={title} />
               </div>
               <h3>{title}</h3>
+              {/* <p>{text}</p> */}
+              <p>{tech}</p>
               <div className="portfolio__item-cta">
                 <a href={github} className="btn btn-primary" target="_blank">
                   Github
                 </a>
-                <a href={demo} className="btn btn-primary" target="_blank">
-                   Demo
+                <a href={livesite} className="btn btn-primary" target="_blank">
+                   Live Site
                 </a>
               </div>
             </article>
--- src/data/data.js
+++ src/data/data.js
@@ -3,8 +3,7 @@ import WeLoveMovies from "../assets/WeLoveMovies.png";
 import FlashCard from "../assets/FlashCard.png";
 import DecoderRing from "../assets/DecoderRing.png";
 import ReservationSystem from "../assets/ReservationSystem.png"
-//import test from "../assets/test.png";
-//import IMG6 from "../assets/portfolio6.jpg";
+
 
 export const data = [
     {
@@ -12,41 +11,53 @@ export const data = [
       image: ReservationSystem,
       title: "Reservation-System-App",
       github: "https://github.com/Artur-Vanivskyi/Restaurant-reservation-system",
-      demo: "https://front-end-three-swart.vercel.app/dashboard",
+      livesite: "https://front-end-three-swart.vercel.app/dashboard",
+      text: "This web app is designed for restaurant staff to organize their reservations.",
+      tech: "React, Express, Node.js, PostgreSQL"
     },
     {
       id: 2,
       image: WeLoveMovies,
       title: "WeLoveMovies",
       github: "https://github.com/Artur-Vanivskyi/WeLoveMovies",
-      demo: "https://tranquil-brushlands-63389.herokuapp.com/",
+      livesite: "https://tranquil-brushlands-63389.herokuapp.com/",
+      text: "This web app is designed for restaurant staff to organize their reservations.",
+      tech: "Node.js, Express, Knex. Version control with Git"
     },
     {
       id: 3,
       image: FlashCard,
       title: "FlashCard",
       github: "https://github.com/Artur-Vanivskyi/Project-Fleshcards",
-      demo: "https://flashcards-project-tau.vercel.app/decks/new",
+      livesite: "https://flashcards-project-tau.vercel.app/decks/new",
+      text: "This web app is designed for restaurant staff to organize their reservations.",
+      tech: "JavaScript, React, Express, Knex, HTML, CSS"
     },
     {
       id: 4,
       image: DecoderRing,
       title: "DecoderRing",
       github: "https://github.com/Artur-Vanivskyi/Decoder-ring-project",
-      demo: "https://decoder-ring-project-silk.vercel.app/",
+      livesite: "https://decoder-ring-project-silk.vercel.app/",
+      text: "This web app is designed for restaurant staff to organize their reservations.",
+      tech: "React, Express, Node.js, PostgreSQL"
     },
     {
       id: 1,
       image: grubDash,
       title: "GrubDash",
       github: "https://github.com/Artur-Vanivskyi/GrubDash-Project/tree/main/src",
-      // demo: "https://github.com/Artur-Vanivskyi/Local_library",
+      // livesite: "https://github.com/Artur-Vanivskyi/Local_library",
+      text: "This web app is designed for restaurant staff to organize their reservations.",
+      tech: "HTML5, Bootstrap, React"
     },
     // {
     //   id: 6,
     //   image: IMG6,
     //   title: "Crypto Currentcy dashboard6",
     //   github: "https://github.com",
-    //   demo: "https://github.com/Artur-Vanivskyi/Local_library",
+    //   livesite: "https://github.com/Artur-Vanivskyi/Local_library",
+    //   text: "This web app is designed for restaurant staff to organize their reservations.",
+    //   tech: "React, Express, Node.js, PostgreSQL"
     // },
   ];
