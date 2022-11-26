ee3f22fdb0193d9c6b227c284708c573ca3a5a54
Artur <vanivskyi.artur@thinkful.com>
2022-11-26 16:53:55 -0500

    added todo project to portfolio

--- src/data/data.js
+++ src/data/data.js
@@ -2,62 +2,62 @@ import grubDash from "../assets/grubDash.png";
 import WeLoveMovies from "../assets/WeLoveMovies.png";
 import FlashCard from "../assets/FlashCard.png";
 import DecoderRing from "../assets/DecoderRing.png";
-import ReservationSystem from "../assets/ReservationSystem.png"
-
+import ReservationSystem from "../assets/ReservationSystem.png";
+import TODO from "../assets/to-do.png";
 
 export const data = [
-    {
-      id: 1,
-      image: ReservationSystem,
-      title: "Reservation-System-App",
-      github: "https://github.com/Artur-Vanivskyi/Restaurant-reservation-system",
-      livesite: "https://front-end-three-swart.vercel.app/dashboard",
-      text: "This web app is designed for restaurant staff to organize their reservations.",
-      tech: "React, Express, Node.js, PostgreSQL"
-    },
-    {
-      id: 2,
-      image: WeLoveMovies,
-      title: "WeLoveMovies",
-      github: "https://github.com/Artur-Vanivskyi/WeLoveMovies",
-      livesite: "https://tranquil-brushlands-63389.herokuapp.com/",
-      text: "This web app is designed for restaurant staff to organize their reservations.",
-      tech: "Node.js, Express, Knex. Version control with Git"
-    },
-    {
-      id: 3,
-      image: FlashCard,
-      title: "FlashCard",
-      github: "https://github.com/Artur-Vanivskyi/Project-Fleshcards",
-      livesite: "https://flashcards-project-tau.vercel.app/decks/new",
-      text: "This web app is designed for restaurant staff to organize their reservations.",
-      tech: "JavaScript, React, Express, Knex, HTML, CSS"
-    },
-    {
-      id: 4,
-      image: DecoderRing,
-      title: "DecoderRing",
-      github: "https://github.com/Artur-Vanivskyi/Decoder-ring-project",
-      livesite: "https://decoder-ring-project-silk.vercel.app/",
-      text: "This web app is designed for restaurant staff to organize their reservations.",
-      tech: "React, Express, Node.js, PostgreSQL"
-    },
-    {
-      id: 1,
-      image: grubDash,
-      title: "GrubDash",
-      github: "https://github.com/Artur-Vanivskyi/GrubDash-Project/tree/main/src",
-      // livesite: "https://github.com/Artur-Vanivskyi/Local_library",
-      text: "This web app is designed for restaurant staff to organize their reservations.",
-      tech: "HTML5, Bootstrap, React"
-    },
-    // {
-    //   id: 6,
-    //   image: IMG6,
-    //   title: "Crypto Currentcy dashboard6",
-    //   github: "https://github.com",
-    //   livesite: "https://github.com/Artur-Vanivskyi/Local_library",
-    //   text: "This web app is designed for restaurant staff to organize their reservations.",
-    //   tech: "React, Express, Node.js, PostgreSQL"
-    // },
-  ];
+  {
+    id: 1,
+    image: ReservationSystem,
+    title: "Reservation-System-App",
+    github: "https://github.com/Artur-Vanivskyi/Restaurant-reservation-system",
+    livesite: "https://front-end-three-swart.vercel.app/dashboard",
+    //text: "This web app is designed for restaurant staff to organize their reservations.",
+    tech: "React, Express, Node.js, PostgreSQL",
+  },
+  {
+    id: 2,
+    image: WeLoveMovies,
+    title: "WeLoveMovies",
+    github: "https://github.com/Artur-Vanivskyi/WeLoveMovies",
+    livesite: "https://tranquil-brushlands-63389.herokuapp.com/",
+    //text: "This web app is designed for restaurant staff to organize their reservations.",
+    tech: "Node.js, Express, Knex. Version control with Git",
+  },
+  {
+    id: 3,
+    image: FlashCard,
+    title: "FlashCard",
+    github: "https://github.com/Artur-Vanivskyi/Project-Fleshcards",
+    livesite: "https://flashcards-project-tau.vercel.app/decks/new",
+    //text: "This web app is designed for restaurant staff to organize their reservations.",
+    tech: "JavaScript, React, Express, Knex, HTML, CSS",
+  },
+  {
+    id: 4,
+    image: DecoderRing,
+    title: "DecoderRing",
+    github: "https://github.com/Artur-Vanivskyi/Decoder-ring-project",
+    livesite: "https://decoder-ring-project-silk.vercel.app/",
+    //text: "This web app is designed for restaurant staff to organize their reservations.",
+    tech: "React, Express, Node.js, PostgreSQL",
+  },
+  {
+    id: 6,
+    image: TODO,
+    title: "Todo list for your fun",
+    github: "https://github.com/Artur-Vanivskyi/TO-DO-List",
+    livesite: "https://client-one-theta.vercel.app/",
+    //text: "This web app is designed for restaurant staff to organize their reservations.",
+    tech: "React, Express, Node.js, PostgreSQL",
+  },
+  {
+    id: 1,
+    image: grubDash,
+    title: "GrubDash",
+    github: "https://github.com/Artur-Vanivskyi/GrubDash-Project/tree/main/src",
+    // livesite: "https://github.com/Artur-Vanivskyi/Local_library",
+    //text: "This web app is designed for restaurant staff to organize their reservations.",
+    tech: "HTML5, Bootstrap, React",
+  },
+];
