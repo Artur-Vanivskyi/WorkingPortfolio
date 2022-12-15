b2bf68a65b80183e39a7ef6ae17d1972fd2b4d77
Jose Campuzano <campuzanojoseluis14@gmail.com>
2022-12-15 13:51:14 -0800

    Added Tools and Additional Skills Cards to Experience Section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -98,6 +98,113 @@ const Experience = () => {
             </article>
           </div>
         </div>
+      <div className="experience__tools">
+          <h3>Tools</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Mocha</h4>
+                {/* <small className="text-light">Experiensed</small> */}
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Chai</h4>
+                {/* <small className="text-light">Experiensed</small> */}
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Knex</h4>
+                {/* <small className="text-light">Experiensed</small> */}
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Git</h4>
+                {/* <small className="text-light">Experiensed</small> */}
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>GitHub</h4>
+                {/* <small className="text-light">Experiensed</small> */}
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Heroku</h4>
+                {/* <small className="text-light">Experiensed</small> */}
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>NPM</h4>
+                {/* <small className="text-light">Experiensed</small> */}
+              </div>
+            </article>
+          </div>
+        </div>
+
+        <div className="experience__skills">
+          <h3>Additional Skills</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Spanish</h4>
+                {/* <small className="text-light">Experiensed</small> */}
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Adobe Suite</h4>
+                {/* <small className="text-light">Experiensed</small> */}
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Microsoft Suite</h4>
+                {/* <small className="text-light">Experiensed</small> */}
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Project Management</h4>
+                {/* <small className="text-light">Experiensed</small> */}
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Leadership</h4>
+                {/* <small className="text-light">Experiensed</small> */}
+              </div>
+            </article>
+          </div>
+        </div>
+
+
       </div>
     </section>
   );
