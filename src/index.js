9e4e11cfe4e6fb9e5feb20d536c60f5251c6e124
Artur Vanivskyi <vanivskyi.artur@gmail.com>
2023-06-15 13:55:51 -0400

    taking com

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
-//comment
+
 ReactDOM.render(
   <Router>
     <App />
